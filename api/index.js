9240c215d4c9b8ddef72b2a3d4425f79db79ac2f
Toshmoney <abdulmaleeqomotosho@gmail.com>
2023-05-08 19:52:59 +0100

    working on getAllWords and getSingleWords

--- api/index.js
+++ api/index.js
@@ -3,16 +3,14 @@ const app = express();
 const port = 3012;
 const bodyParser = require("body-parser");
 const definition = require("./data/definitions");
-
+const router = require("./routes/handler");
 
 
 
 // ******* Middlewares *********
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get("/api/v1/parenthesis", (req, res)=>{
-    res.status(200).json(definition[0].parenthesis)
-})
+app.use(router)
 app.listen(port, ()=>{
     console.log(`Coding dictionary server started on port ${port}`);
 })
