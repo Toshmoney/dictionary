d88bfd946e59309f06b26e4b459a631ccc9472cb
olaniyigd <gbadegesinpharuq@yahoo.com>
2023-05-08 16:46:31 +0100

    updating api server

---
+++ api/data/definitions.js
@@ -0,0 +1,51 @@
+const definition = [
+    {
+        //a
+        //b
+        //c
+        "curly braces":[
+            {
+                "word": "parenthesis",
+                "meaning": ""
+            }
+        ],
+        //d
+        //e
+        "emphasis":[
+            {
+                "word": "emphasis",
+                "meaning": ""
+            }
+        ],
+        //f
+        //g
+        //h
+        //i
+        //j
+        //k
+        //l
+        //m
+        //n
+        //0
+        //p
+        "parenthesis":[
+            {
+                "word": "parenthesis",
+                "meaning": ""
+            }
+        ]
+        //q
+        //r
+        //r
+        //s
+        //t
+        //w
+        //x
+        //y
+        //z
+
+
+
+    }
+]
+module.exports = definition;
---
+++ api/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3012;
+const bodyParser = require("body-parser");
+const definition = require("./data/definitions");
+
+
+
+
+// ******* Middlewares *********
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/api/v1/parenthesis", (req, res)=>{
+    res.status(200).json(definition[0].parenthesis)
+})
+app.listen(port, ()=>{
+    console.log(`Coding dictionary server started on port ${port}`);
+})
